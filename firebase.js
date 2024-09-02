6fb65019d0b2cb63717adbf1e693dde2128bbc59
Edgar Jurkoit <90857989+edgaros24@users.noreply.github.com>
2024-09-02 17:34:39 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3SOi6zYGdrOJ7o2XMTksh4ic70azHXxw",
-  authDomain: "compadproject.firebaseapp.com",
-  projectId: "compadproject",
-  storageBucket: "compadproject.appspot.com",
-  messagingSenderId: "365207133835",
-  appId: "1:365207133835:web:91bf3b600671de0b22be8e",
-  measurementId: "G-RV2S6W44CR"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
